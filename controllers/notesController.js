49fe765611cf5afb8d6de9b611b91a290eea593c
Jared Malan <jared.malan7@gmail.com>
2024-01-27 12:26:02 -0700

    getSingleNote Request fully functioning

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,6 +1,8 @@
 const mongodb = require('../db/database')
 const ObjectId = require('mongodb').ObjectId
 
+
+//GET all the notes from the database 
 const getAllNotes = async (req, res) => {
     try {
         const result = await mongodb.getDatabase().db().collection('Notes').find({});
@@ -15,7 +17,30 @@ const getAllNotes = async (req, res) => {
 }
 
 
+//GET a single note from the database by ObjectId
+const getSingleNote = async (req, res) => {
+    try {
+        const noteId = req.params.id;
+        if (!ObjectId.isValid(noteId)) {
+            return res.status(400).json({ error: 'Invalid ObjectId' });
+        }
+
+        const result = await mongodb.getDatabase().db().collection('Notes').findOne({ _id: new ObjectId(noteId) })
+
+        if (!result) {
+            return res.status(404).json({ error: 'Note not found' })
+        }
+
+        console.log('Retrieved Note:', result)
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result)
+    } catch (error) {
+        console.error('Error retrieving Note:', error);
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+}
 
 module.exports = {
-    getAllNotes
+    getAllNotes,
+    getSingleNote,
 } 
