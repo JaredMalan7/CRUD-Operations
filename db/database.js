f8cba2c353e109f41e8899335d79a2e1cb58e693
Jared Malan <jared.malan7@gmail.com>
2024-02-03 10:17:32 -0700

    middleware deleted, database mongodb function updated with vaidation to avoid conflict

--- db/database.js
+++ db/database.js
@@ -3,19 +3,23 @@ const MongoClient = require('mongodb').MongoClient
 
 let database
 const initDb = (callback) => {
-    if (database) {
-        console.log('Database is already initiated!')
+
+    //Validation to check if the database is initialized
+    if (database && database.topology.isConnected()) {
+        console.log('Database is already initiated and connected!')
         return callback(null, database)
     }
 
     const mongoUrl = process.env.MONGO_URL
 
+    // validation to check if the MON_URL was provided
     if (!mongoUrl) {
         const errorMessage = 'MONGO_URL not provided in environment variables'
         console.error(errorMessage)
         return callback(new Error(errorMessage))
     }
 
+    //Validation to check if the connection with the database was successful
     MongoClient.connect(mongoUrl)
         .then((client) => {
             database = client
@@ -28,16 +32,27 @@ const initDb = (callback) => {
         })
 }
 
+const checkDatabaseConnection = (req, res, next) => {
+    //Checks database Connection
+    if (!database || !database.topology.isConnected()) {
+        const errorMessage = 'Database not initialized or not connected'
+        console.error(errorMessage)
+        return res.status(500).json({ error: 'Internal Server Error' })
+    }
+    next()
+}
+
 
 
 const getDatabase = () => {
-    if (!database) {
-        throw Error('Database not initialized')
+    if (!database || !database.topology.isConnected()) {
+        throw Error('Database not initialized or not connected')
     }
     return database
 }
 
 module.exports = {
     initDb,
+    checkDatabaseConnection,
     getDatabase
 }
