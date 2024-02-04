843932cf9da956c956e2e9a71e56104330ba5fef
Jared Malan <jared.malan7@gmail.com>
2024-02-03 22:44:12 -0700

    Documentation link fixed

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/login', passport.authenticate('github'), (req, res) => {
     //  #swagger.tags = ['Authentication'] = {
     /* #swagger.description = "This authentication uses the GitHub validation that redirects to a callback URL. Swagger UI, by default, doesn't handle authentication like logging through OAuth or other similar methods.<br>
     1. Open a separate browser window or tab.<br>
-    2. Navigate to the /login route: http://localhost:3000/login <br>
+    2. Navigate to the /login route: https://cru-operations-authenticated.onrender.com/login <br>
     3. The page will redirect you to the GitHub login page.<br>
     4. Log in with your GitHub credentials.<br>
     5. GitHub will redirect you back to the application's home page. */
